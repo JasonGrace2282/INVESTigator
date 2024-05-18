15c06011d46c43ca7d3481270e0a0cee1334f9c3
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-05-18 14:06:30 -0400

    Fix username in dashboard

--- case/views.py
+++ case/views.py
@@ -12,6 +12,7 @@ class Dashboard(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["cases"] = Case.objects.all()
+        context["user"] = self.request.user
         return context
 
     def get(self, request, *args, **kwargs):
